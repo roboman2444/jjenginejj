ecfaeb152940b4eaa00669b617fbf3ee12a5bf61
roboman2444 <roboman2444@gmail.com>
2013-08-05 19:27:50 -0400

    ready for moving

--- src/camera.java
+++ src/camera.java
@@ -5,7 +5,7 @@ public class camera{
 	public static float fov = 90;
 	public static float posx = 0;
 	public static float posy = 0;
-	public static float posz = -5;
+	public static float posz = 5;
 	public static float rotx = 0;
 	public static float rotz = 0;
 	public static float roty = 0;//not used in a pan/tilt style
--- src/model.java
+++ src/model.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import objimp.*;
 
 public class model {
 	//todo
